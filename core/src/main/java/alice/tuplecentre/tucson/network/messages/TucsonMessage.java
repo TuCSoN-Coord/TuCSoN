fbdd2639404b088b53229258db7f6cd8558d827d
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-03-12 14:19:22 +0100

    - Renamed TucsonMsg to TucsonMessage - Made TucsonMsgGeneric class, abstract

--- core/src/main/java/alice/tuplecentre/tucson/network/messages/TucsonMessage.java
+++ core/src/main/java/alice/tuplecentre/tucson/network/messages/TucsonMessage.java
@@ -4,7 +4,13 @@ import alice.tuplecentre.tucson.network.messages.events.EventMsg;
 
 /**
  * // TODO: 11/03/2018 add docs
+ *
+ * @author Nicola Piscaglia
  */
-public interface TucsonMsg extends Message {
+public interface TucsonMessage extends Message {
+
+    /**
+     * @return the event message associated to this message
+     */
     EventMsg getEventMsg();
 }
